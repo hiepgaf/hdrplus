8188ff384403deee43dc097f9b2dbeaf23816dbc
whqwill <whqwill@gmail.com>
2018-05-08 14:20:33 +0800

    first commit

---
+++ build/CMakeFiles/hdrplus.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "/home/haiqing/Project/hdr-plus/src/HDRPlus.cpp" "/home/haiqing/Project/hdr-plus/build/CMakeFiles/hdrplus.dir/src/HDRPlus.cpp.o"
+  "/home/haiqing/Project/hdr-plus/src/align.cpp" "/home/haiqing/Project/hdr-plus/build/CMakeFiles/hdrplus.dir/src/align.cpp.o"
+  "/home/haiqing/Project/hdr-plus/src/finish.cpp" "/home/haiqing/Project/hdr-plus/build/CMakeFiles/hdrplus.dir/src/finish.cpp.o"
+  "/home/haiqing/Project/hdr-plus/src/merge.cpp" "/home/haiqing/Project/hdr-plus/build/CMakeFiles/hdrplus.dir/src/merge.cpp.o"
+  "/home/haiqing/Project/hdr-plus/src/util.cpp" "/home/haiqing/Project/hdr-plus/build/CMakeFiles/hdrplus.dir/src/util.cpp.o"
+  )
+set(CMAKE_CXX_COMPILER_ID "GNU")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  "/home/haiqing/Project/Halide/include"
+  "/home/haiqing/Project/Halide/tools"
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/hdrplus.dir/cmake_clean.cmake
@@ -0,0 +1,14 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/hdrplus.dir/src/HDRPlus.cpp.o"
+  "CMakeFiles/hdrplus.dir/src/align.cpp.o"
+  "CMakeFiles/hdrplus.dir/src/finish.cpp.o"
+  "CMakeFiles/hdrplus.dir/src/merge.cpp.o"
+  "CMakeFiles/hdrplus.dir/src/util.cpp.o"
+  "hdrplus.pdb"
+  "hdrplus"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/hdrplus.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
